200e5e87692608039aae70d73ea64ec621badc0e
Jana <pi.pekarkova@seznam.cz>
2021-05-02 20:21:40 +0200

    Přidání funkcí mazání a přidávání vizitek

--- src/main/java/cz/czechitas/java2webapps/ukol3/entity/Vizitka.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/entity/Vizitka.java
@@ -6,84 +6,84 @@ import java.util.Objects;
  * Entita obsahující údaje zobrazené na vizitce.
  */
 public class Vizitka {
-  private String jmeno;
-  private String firma;
-  private String ulice;
-  private String obecPsc;
-  private String email;
-  private String telefon;
-  private String web;
+    private String jmeno;
+    private String firma;
+    private String ulice;
+    private String obecPsc;
+    private String email;
+    private String telefon;
+    private String web;
 
-  public Vizitka() {
-  }
+    public Vizitka() {
+    }
 
-  public Vizitka(String jmeno, String firma, String ulice, String obecPsc, String email, String telefon, String web) {
-    this.jmeno = Objects.requireNonNull(jmeno);
-    this.firma = Objects.requireNonNull(firma);
-    this.ulice = Objects.requireNonNull(ulice);
-    this.obecPsc = Objects.requireNonNull(obecPsc);
-    this.email = email;
-    this.telefon = telefon;
-    this.web = web;
-  }
+    public Vizitka(String jmeno, String firma, String ulice, String obecPsc, String email, String telefon, String web) {
+        this.jmeno = Objects.requireNonNull(jmeno);
+        this.firma = Objects.requireNonNull(firma);
+        this.ulice = Objects.requireNonNull(ulice);
+        this.obecPsc = Objects.requireNonNull(obecPsc);
+        this.email = email;
+        this.telefon = telefon;
+        this.web = web;
+    }
 
-  public String getJmeno() {
-    return jmeno;
-  }
+    public String getJmeno() {
+        return jmeno;
+    }
 
-  public void setJmeno(String jmeno) {
-    this.jmeno = Objects.requireNonNull(jmeno);
-  }
+    public void setJmeno(String jmeno) {
+        this.jmeno = Objects.requireNonNull(jmeno);
+    }
 
-  public String getFirma() {
-    return firma;
-  }
+    public String getFirma() {
+        return firma;
+    }
 
-  public void setFirma(String firma) {
-    this.firma = Objects.requireNonNull(firma);
-  }
+    public void setFirma(String firma) {
+        this.firma = Objects.requireNonNull(firma);
+    }
 
-  public String getUlice() {
-    return ulice;
-  }
+    public String getUlice() {
+        return ulice;
+    }
 
-  public void setUlice(String ulice) {
-    this.ulice = Objects.requireNonNull(ulice);
-  }
+    public void setUlice(String ulice) {
+        this.ulice = Objects.requireNonNull(ulice);
+    }
 
-  public String getObecPsc() {
-    return obecPsc;
-  }
+    public String getObecPsc() {
+        return obecPsc;
+    }
 
-  public void setObecPsc(String obecPsc) {
-    this.obecPsc = Objects.requireNonNull(obecPsc);
-  }
+    public void setObecPsc(String obecPsc) {
+        this.obecPsc = Objects.requireNonNull(obecPsc);
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public String getTelefon() {
-    return telefon;
-  }
+    public String getTelefon() {
+        return telefon;
+    }
 
-  public void setTelefon(String telefon) {
-    this.telefon = telefon;
-  }
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
 
-  public String getWeb() {
-    return web;
-  }
+    public String getWeb() {
+        return web;
+    }
 
-  public void setWeb(String web) {
-    this.web = web;
-  }
+    public void setWeb(String web) {
+        this.web = web;
+    }
 
-  public String getCelaAdresa() {
-    return ulice + ", " + obecPsc;
-  }
+    public String getCelaAdresa() {
+        return ulice + ", " + obecPsc;
+    }
 }
